ae06b50371946d7f253344c8569f9ac1d2a9c7a9
xiawang1024 <xiawang1024@gmail.com>
2023-06-13 16:38:51 +0800

    feat: ✨ 流量表

--- src/components/big/rightTop.vue
+++ src/components/big/rightTop.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: xiawang1024
  * @Date: 2023-06-12 16:06:13
- * @LastEditTime: 2023-06-13 16:17:45
+ * @LastEditTime: 2023-06-13 16:37:46
  * @LastEditors: xiawang1024
  * @Description:
  * @FilePath: /electronic-file/src/components/big/rightTop.vue
@@ -19,20 +19,15 @@ export default {
   data() {
     return {
       config: {
-        header: ['列1', '列2', '列3'],
-        data: [
-          ['行1列1', '行1列2', '行1列3'],
-          ['行2列1', '行2列2', '行2列3'],
-          ['行3列1', '行3列2', '行3列3'],
-          ['行4列1', '行4列2', '行4列3'],
-          ['行5列1', '行5列2', '行5列3'],
-          ['行6列1', '行6列2', '行6列3'],
-          ['行7列1', '行7列2', '行7列3'],
-          ['行8列1', '行8列2', '行8列3'],
-          ['行9列1', '行9列2', '行9列3'],
-          ['行10列1', '行10列2', '行10列3'],
+        header: [
+          '温度(&#8451;)',
+          '压力(kPa)',
+          '瞬时流量(m&sup3;/h)',
+          '累计流量(m&sup3;)',
+          '脉冲(Hz)',
         ],
-        index: true,
+        data: [],
+        // index: true,
         rowNum: 6,
         align: ['center'],
       },
@@ -44,7 +39,24 @@ export default {
   methods: {
     getData() {
       Service.flow({}).then(res => {
-        this.config.data = res.data
+        let { code, rows } = res.data
+        if (code == 200) {
+          let arr = []
+          for (let i = 0; i < rows.length; i++) {
+            let item = []
+            item.push(
+              rows[i].temperature,
+              rows[i].pressure,
+              rows[i].flowInstant,
+              rows[i].flowTotal,
+              rows[i].pulse
+            )
+            arr.push(item)
+          }
+          this.config.data = arr
+
+          this.config = { ...this.config }
+        }
       })
     },
   },
