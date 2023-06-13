897928aa5b52c35b7972151fe32359c0deb28e22
xiawang1024 <xiawang1024@gmail.com>
2023-06-13 17:57:07 +0800

    feat: ✨ login api

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,21 @@
 /*
  * @Author: xiawang1024
  * @Date: 2023-06-12 19:10:13
- * @LastEditTime: 2023-06-13 17:31:30
+ * @LastEditTime: 2023-06-13 17:56:43
  * @LastEditors: xiawang1024
  * @Description:
  * @FilePath: /electronic-file/src/api/index.js
  * 工作，生活，健康
  */
 import request from './request'
+
+export const login = ({ username, password }) =>
+  request({
+    url: '/login',
+    method: 'post',
+    data: { username, password },
+  })
+
 // 第一行指标
 export const headInfo = () =>
   request({
